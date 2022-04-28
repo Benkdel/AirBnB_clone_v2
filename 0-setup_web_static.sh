9ca718cf2a3b23d2ac6567c9d2919ca0d400b8bc
Carlos Bentin <cbentin12@gmail.com>
2022-04-27 21:12:14 -0500

    fix: symbolic link using PWD before target path

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,10 +9,10 @@ mkdir -p data/web_static/shared/
 echo "TEST web static deployment" > data/web_static/releases/test/index.html
 
 # create symbolic link to test folder
-ln -sf /data/web_static/releases/test /data/web_static/current
+sudo ln -sf $PWD/data/web_static/releases/test/ data/web_static/current
 
 # give full ownership of the data directory to ubuntu and group
-sudo chown -R ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu data
 
 #
 # nginx configuration
@@ -23,17 +23,17 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install nginx -y
 
 # redirections - error page
-sudo sed -i "s/^server\s{/server {\n\t\terror_page 404 \/error_simple.html;/1" /etc/nginx/sites-available/default
+sudo sed -i "s/^server\s{/server {\n\terror_page 404 \/error_simple.html;/1" /etc/nginx/sites-available/default
 
 # configure response header
 sudo sed -i "/^server/a \\\tadd_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
 
 # replace text: sed syntax is: 's!oldcontent!newcontent!' file.txt
 # use s! instead of s/ to avoid having to scape characters
-sed -i 's!root /var/www/html;!root /data/web_static/current/;!' nginxTestFile
+sudo sed -i 's!root /var/www/html;!root /data/web_static/current/;!' /etc/nginx/sites-available/default
 
 # append text: sed syntax is: '/^anothervalue=.*/a after=me' file.txt THIS IS NOT WORKING....
-sed -i '/^\tserver_name/ a\\tlocation /hbnb_static \{\n\t\talias /data/web_static/current;\n\t\}\n' nginxTestFile #/etc/nginx/sites-available/default
-.
+sudo sed -i '/^\tserver_name/ a\\tlocation /hbnb_static \{\n\t\talias /data/web_static/current;\n\t\}\n' /etc/nginx/sites-available/default
+
 ### Start service nginx.
 sudo service nginx restart
