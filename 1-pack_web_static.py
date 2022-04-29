50abf1e848715bf1aeb5d7a13ac94040b1ba9481
Carlos Bentin <cbentin12@gmail.com>
2022-04-28 23:42:30 -0500

    task 1 completed

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 
+    Fabric script generates a .tgz archive of
+    web_static folder 
+"""
+from fabric.operations import local
+from datetime import datetime
+
+
+def do_pack():
+    """
+        All archives must be stored in the folder versions
+    """
+    local("mkdir -p versions")
+    status = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if status.failed:
+        return None
+    print(status)
+    return status
