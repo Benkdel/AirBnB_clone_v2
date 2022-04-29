b768163dde97076c707a322fbadb8e3832151cc1
Carlos Bentin <cbentin12@gmail.com>
2022-04-29 14:43:34 -0500

    adding remaining tasks

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,1 +1,43 @@
 #!/usr/bin/python3
+"""
+    Task 2
+"""
+
+
+from fabric.api import env, put, run
+from os.path import exists
+import shlex
+env.hosts = ['35.185.108.180', '34.229.169.234']
+env.user = 'ubuntu'
+
+
+def do_deploy(archive_path):
+    """
+        All remote commands must be executed on your both web servers
+    """
+    if not exists(archive_path):
+        return False
+    try:
+        pname = archive_path.replace('/', ' ')
+        pname = shlex.split(pname)
+        pname = pname[-1]
+
+        wname = pname.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
+
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(pname)
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(releases_path))
+        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
+        run("rm {}".format(tmp_path))
+        run("mv {}web_static/* {}".format(releases_path, releases_path))
+        run("rm -rf {}web_static".format(releases_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(releases_path))
+        print("New version deployed!")
+        return True
+    except BaseException:
+        return False
